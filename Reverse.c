ca800d336e6007cfd1e4822dca593b92d55d1f76
Bhumika A Gowda <bhumikaa851@gmail.com>
2024-11-26 22:19:36 +0530

    Create Reverse.c

---
+++ Reverse.c
@@ -0,0 +1,12 @@
+#include stdioo.h>
+int main() {
+    int i=5;
+    do{
+        printf("%d\n",i);
+        i--;
+        }
+        while(i>0);
+    {printf("Bhumika A");
+     }
+    return 0;
+    }
