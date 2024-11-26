cc9e0e685e81b20fa297cc18cd0634554b929429
Bhumika A Gowda <bhumikaa851@gmail.com>
2024-11-26 22:26:35 +0530

    Create 1to10dowhile.c

---
+++ 1to10dowhile.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int i=5;
+    do{
+        printf("%d\n",i);
+        i++;
+        }
+        while(i<=10);
+    {
+        printf("Bhumika A");
+     }
+    return 0;
+    }
