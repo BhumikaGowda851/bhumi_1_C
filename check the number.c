a83a7ba21d2a9b94a8b2330f38d16ad5d72206db
Bhumika A Gowda <bhumikaa851@gmail.com>
2024-11-26 12:32:56 +0530

    Create check the number.c

---
+++ check the number.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+  int num;
+printf("enter the number");
+scanf("%d",&num);
+if (number>0)
+{
+printf("the number is positive.\n");
+}
+else if (number<0)
+{
+printf("the number is negative.\n");
+}
+return 0;
+}
